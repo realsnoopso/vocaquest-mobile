2b33d51eda463369862247a0d3677c73c2ade64e
snoopso <realsnoopso@users.noreply.github.com>
2026-04-30 19:57:24 +0900

    fix: withPodfile using .contents for Swift concurrency

--- plugins/withSwiftConcurrency.js
+++ plugins/withSwiftConcurrency.js
@@ -1,20 +1,26 @@
-const { withPlugins, withXcodeProject } = require('expo/config-plugins');
+const { withPodfile } = require('expo/config-plugins');
 
-const withSwiftStrictConcurrency = (config) => {
-  return withXcodeProject(config, (config) => {
-    const xcodeProject = config.modResults;
+module.exports = (config) => {
+  return withPodfile(config, (config) => {
+    const podfile = config.modResults.contents;
     
-    // Set SWIFT_STRICT_CONCURRENCY to minimal for all build configurations
-    const configurations = xcodeProject.pbxXCBuildConfigurationSection();
-    for (const key in configurations) {
-      const buildSettings = configurations[key].buildSettings;
-      if (buildSettings) {
-        buildSettings['SWIFT_STRICT_CONCURRENCY'] = 'minimal';
-      }
+    // Add SWIFT_STRICT_CONCURRENCY to the post_install block
+    const swiftFix = `
+  # Fix Swift 6 concurrency warnings in Xcode 16
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['SWIFT_STRICT_CONCURRENCY'] = 'minimal'
+    end
+  end
+`;
+    
+    if (!podfile.includes('SWIFT_STRICT_CONCURRENCY')) {
+      config.modResults.contents = podfile.replace(
+        /(post_install do \|installer\|)/,
+        '$1' + swiftFix
+      );
     }
     
     return config;
   });
 };
-
-module.exports = withSwiftStrictConcurrency;
