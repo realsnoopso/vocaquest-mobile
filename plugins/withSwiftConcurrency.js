8a419187547888ebc0b3c538a3ddb3745a815464
snoopso <realsnoopso@users.noreply.github.com>
2026-04-30 19:49:40 +0900

    fix: use withXcodeProject for Swift concurrency build settings

--- plugins/withSwiftConcurrency.js
+++ plugins/withSwiftConcurrency.js
@@ -1,37 +1,20 @@
-const { withDangerousMod } = require('expo/config-plugins');
-const fs = require('fs');
-const path = require('path');
+const { withPlugins, withXcodeProject } = require('expo/config-plugins');
 
-module.exports = function withSwiftConcurrency(config) {
-  return withDangerousMod(config, [
-    'ios',
-    async (config) => {
-      const projectRoot = config.modRequest.platformProjectRoot;
-      // Add SWIFT_STRICT_CONCURRENCY to all pod targets via a separate ruby script
-      const podfilePath = path.join(projectRoot, 'Podfile');
-      
-      if (fs.existsSync(podfilePath)) {
-        let podfile = fs.readFileSync(podfilePath, 'utf8');
-        
-        const swiftFix = `
-  # Fix Swift 6 concurrency errors in Xcode 16
-  installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      config.build_settings['SWIFT_STRICT_CONCURRENCY'] = 'minimal'
-    end
-  end
-`;
-        
-        if (!podfile.includes('SWIFT_STRICT_CONCURRENCY')) {
-          podfile = podfile.replace(
-            /(post_install do \|installer\|)/m,
-            '$1' + swiftFix
-          );
-          fs.writeFileSync(podfilePath, podfile);
-        }
+const withSwiftStrictConcurrency = (config) => {
+  return withXcodeProject(config, (config) => {
+    const xcodeProject = config.modResults;
+    
+    // Set SWIFT_STRICT_CONCURRENCY to minimal for all build configurations
+    const configurations = xcodeProject.pbxXCBuildConfigurationSection();
+    for (const key in configurations) {
+      const buildSettings = configurations[key].buildSettings;
+      if (buildSettings) {
+        buildSettings['SWIFT_STRICT_CONCURRENCY'] = 'minimal';
       }
-      
-      return config;
-    },
-  ]);
+    }
+    
+    return config;
+  });
 };
+
+module.exports = withSwiftStrictConcurrency;
